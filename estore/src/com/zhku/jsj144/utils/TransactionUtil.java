16ad52bc97c1f132c76d61f2930155d1c8385430
Forever99 <2406045700@qq.com>
2017-11-22 15:35:59 +0800

    ThreadLocal类，添加说明

--- estore/src/com/zhku/jsj144/utils/TransactionUtil.java
+++ estore/src/com/zhku/jsj144/utils/TransactionUtil.java
@@ -5,6 +5,24 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 //把得到连接及事务有关的方法写到此类中
+/*
+ *  ThreadLocal类【线程本地类】的使用：确保同一个线程使用的始终是同一个Connection对象
+ *  
+ *  ThreadLocal类:  内部是维护了 一个 map , 这个map 的key 始终 都是 当前 的线程 
+ *  	key:当前线程    
+ * 	    value:connection对象
+ * 为什么使用这个类：
+ * 因为进行事务处理时，要开启事务，提交事务，回滚事务
+ *       而因为操作数据库时，是从连接池中获取到的连接：connection，
+ *       一般的事务处理，我们无法确定我们拿到的是不是同一个connection，如果是不同的，我们的事务管理就会失败；
+ *       所以为了确保拿到的是同一个connection，我们需要处理connection的唯一性问题？
+ *       而ThreadLocal类就解决了这个问题
+ *       
+ *--ThreadLocal则从另一个角度来解决多线程的并发访问。
+ *--ThreadLocal会为每一个线程提供一个独立的变量副本，从而隔离了多个线程对数据的访问冲突。
+ *--因为每一个线程都拥有自己的变量副本，从而也就没有必要对该变量进行同步了。
+ *--ThreadLocal提供了线程安全的共享对象，在编写多线程代码时，可以把不安全的变量封装进ThreadLocal。
+ */
 public class TransactionUtil {
 	
 	// 内部是维护了 一个 map , 这个map 的key 始终 都是 当前 的线程 
